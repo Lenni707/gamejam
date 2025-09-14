fca6dfcbe142cd7cb299559fc99fe83800ffafee
Anwender <kokoloko.gaming@gmail.com>
2025-09-14 19:02:40 +0200

    title scene

---
+++ scripts/title_screen.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_texture_button_pressed() -> void: # play
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")	
+
+
+func _on_texture_button_2_pressed() -> void: # quit
+	get_tree().quit()
