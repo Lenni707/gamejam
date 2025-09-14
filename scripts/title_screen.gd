dd16d09d511b7d5877606414d9c80ab32ea603ed
Anwender <kokoloko.gaming@gmail.com>
2025-09-14 21:04:27 +0200

    explaing scene

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_texture_button_pressed() -> void: # play
-	get_tree().change_scene_to_file("res://Scenes/game.tscn")	
+	get_tree().change_scene_to_file("res://Scenes/explaining.tscn")	
 
 
 func _on_texture_button_2_pressed() -> void: # quit
