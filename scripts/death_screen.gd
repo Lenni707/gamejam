4a72de1e3c5d978bd533460a96c933b82f773a32
Kokomolokolo <kokoloko.gaming@gmail.com>
2025-09-22 22:46:33 +0200

    integration http requests für localhost

--- scripts/death_screen.gd
+++ scripts/death_screen.gd
@@ -7,3 +7,16 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
 	
+func send_scores():
+	var http = HTTPRequest.new()
+	add_child(http)
+	
+	var survival_time = GameState.last_time
+	
+	var json_data = JSON.stringify({
+		"survival_time": survival_time
+	})
+	
+	var headers = ["Content-Type: application/json"]
+	
+	http.request("https://localhost:8000/api/save-score", headers, HTTPClient.METHOD_POST, json_data)
