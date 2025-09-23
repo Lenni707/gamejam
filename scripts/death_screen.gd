14dd0924204574a2eed3e9a9654ba1c2c6ed426a
Lenni <gamer_like_you@gmx.de>
2025-09-23 21:28:08 +0200

    hello world

--- scripts/death_screen.gd
+++ scripts/death_screen.gd
@@ -1,22 +1,28 @@
 extends Control
 
 @onready var final_time: Label = $Label3
+@onready var http: HTTPRequest = $HTTPRequest
+
+func _ready() -> void:
+	final_time.text = GameState.last_time
+	add_child(http)
+	http.request_completed.connect(_on_http_request_request_completed)
+	send_data()
 
 func _process(delta: float) -> void:
-	final_time.text = GameState.last_time
 	if Input.is_action_just_pressed("ui_accept"):
 		get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
 	
-func send_scores():
-	var http = HTTPRequest.new()
-	add_child(http)
-	
-	var survival_time = GameState.last_time
-	
-	var json_data = JSON.stringify({
-		"survival_time": survival_time
-	})
-	
-	var headers = ["Content-Type: application/json"]
-	
-	http.request("https://localhost:8000/api/save-score", headers, HTTPClient.METHOD_POST, json_data)
+
+func send_data() -> void:
+	var url := "http://127.0.0.1:5000/fetch"
+	var headers := ["Content-Type: application/json"]
+	var body := {"result": GameState.last_time}
+	var json := JSON.stringify(body)
+	var err := http.request(url, headers, HTTPClient.METHOD_POST, json)
+	if err != OK:
+		push_error("HTTPRequest failed to start: %s" % err)
+
+func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	var text := body.get_string_from_utf8()
+	print("HTTP result:", result, " status:", response_code, " body:", text)
